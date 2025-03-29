5512bc390a6cd12db59770627589c299c8ea380e
intsuc <s@intsuc.dev>
2025-03-29 20:22:09 +0900

    Remove persister

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,18 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
-import { QueryClient } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "./components/theme-provider.tsx"
 import { App } from "./app.tsx"
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      gcTime: 1000 * 60 * 60 * 24,
-    },
-  },
-})
-
-const persister = createSyncStoragePersister({
-  storage: window.localStorage,
-})
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={{ persister }}
-    >
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <App />
       </ThemeProvider>
-    </PersistQueryClientProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
