98f88696494ac034c7ded1c3aca3701a5e1b2a75
intsuc <s@intsuc.dev>
2025-04-01 00:05:49 +0900

    Add vercel.json

--- next.config.ts
+++ next.config.ts
@@ -9,19 +9,6 @@ const nextConfig: NextConfig = {
   experimental: {
     useCache: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/:key",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "s-maxage=60, stale-while-revalidate=120",
-          },
-        ],
-      },
-    ];
-  },
 }
 
 export default nextConfig
